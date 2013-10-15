f497ed4a40154227bea6003e9c7f60d9a8be2271
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-10-15 23:43:57 +0200

    basic animation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,5 +18,20 @@ define([], function () {
         $('html,body').animate({scrollTop:dest}, 1000,'swing');
     });
 
+    var active = false;
+    $('.circles').mouseover( function(event) {
+        if(!active) {
+            $(this).stop().animate({opacity: 0.1}, 500);
+        }
+    }).mouseout( function(event) {
+        if(!active) {
+            $(this).stop().animate({opacity: 1.0}, 500);
+        }
+    }).click( function(event) {
+        $('.circles').animate({opacity: 1.0}, 500);
+        $(this).stop().css({opacity: 0.1}, 500);
+        active = true;
+    });
+
     return '\'Allo \'Allo!';
 });
