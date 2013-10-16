58376996e13f211836b8cdc1100c413149884d55
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-10-16 17:06:01 +0200

    finalized design update v0.3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,11 +2,9 @@
 define([], function () {
     'use strict';
 
-    // $(function() {
-    // });
-
-    $('.scroll-link').click(function(event){
-        event.preventDefault();
+    // Smooth scroll
+    $('.scroll-link').click(function(e){
+        e.preventDefault();
         //calculate destination place
         var dest=0;
         if($(this.hash).offset().top > $(document).height()-$(window).height()){
@@ -18,20 +16,41 @@ define([], function () {
         $('html,body').animate({scrollTop:dest}, 1000,'swing');
     });
 
-    var active = false;
-    $('.circles').mouseover( function(event) {
-        if(!active) {
-            $(this).stop().animate({opacity: 0.1}, 500);
+    // Navigation
+    var isActive = false;
+    $('.circles').mouseover( function() {
+        if(!isActive) {
+            $('img, > .text-wrapper', this).stop().animate({'opacity': '0.3'}, 300);
         }
-    }).mouseout( function(event) {
-        if(!active) {
-            $(this).stop().animate({opacity: 1.0}, 500);
+    }).mouseout( function() {
+        if(!isActive) {
+            $('img, > .text-wrapper', this).stop().animate({'opacity': '1.0'}, 300);
         }
-    }).click( function(event) {
-        $('.circles').animate({opacity: 1.0}, 500);
-        $(this).stop().css({opacity: 0.1}, 500);
-        active = true;
+    }).click( function() {
+        $(this).addClass('circles-selected');
+        $('.circles > img, .circles > .text-wrapper').not(this).animate({'opacity': '0.5'}, 400);
+        $('img, > .text-wrapper', this).stop().animate({'opacity': '0.08'}, 400);
+        $('.listing').removeClass('selected');
+        $('.listing', this).addClass('selected');
+        $('.listing').not('.selected').animate({'opacity': '0.0', 'top': '0px'}, 200).css('visibility', 'hidden');
+        $('.listing', this).css({'visibility': 'visible', 'opacity': '0.0'}).animate({'opacity': '1.0', 'top': '40px'}, 400);
+        isActive = true;
+    });
+    $(document).mouseup(function (e) {
+        var container = $('.circles');
+        // if the target of the click isn't the container...
+        // ... nor a descendant of the container
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            $('.listing').animate({'opacity': '0.0', 'top': '0px'}, {
+                duration: 200,
+                complete: function() {
+                    $(this).css({'top': '0px', 'visibility': 'hidden'});
+                }
+            });
+            $('.circles > img, .circles > .text-wrapper').animate({'opacity': '1.0', 'margin-top': '0px'}, 1000);
+            isActive = false;
+        }
     });
 
-    return '\'Allo \'Allo!';
+    return 'Fourthbit v0.3';
 });
